bd910bd3dd89ecd88c98d01dd65effaf3582ea34
Rahul <rahul@sniper.local>
2026-06-15 17:01:31 +0000

    first commit - base spring boot setup with postgres config

---
+++ cashflow-settlement-engine/src/test/java/com/cashflow/CashflowSettlementEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cashflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashflowSettlementEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
